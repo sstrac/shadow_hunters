ca40474e7cca5dc1e21913a53b93fd6bb4bb0c24
sstrac <s.r.strachan@outlook.com>
2025-10-11 00:28:04 +0100

    animated player

--- Scripts/player_character_body_2d.gd
+++ Scripts/player_character_body_2d.gd
@@ -1,12 +1,25 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed: int = 100
+@onready var anim_tree: AnimationTree = get_node("AnimationTree")
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
-func _physics_process(delta):
-	print(position)
+
+func _physics_process(_delta):
 	get_input()
+	handle_animation()
 	move_and_slide()
+
+
+func handle_animation():
+	if velocity == Vector2.ZERO:
+		anim_tree.get('parameters/playback').travel('Idle')
+		
+	else:
+		anim_tree.get('parameters/playback').travel('Walk')
+		anim_tree.set('parameters/Idle/blend_position', sign(velocity))
+		anim_tree.set('parameters/Walk/blend_position', sign(velocity))
+	
