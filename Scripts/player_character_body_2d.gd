785744ab351d883837a0082bdd1687722f9b2edd
sstrac <s.r.strachan@outlook.com>
2025-10-11 23:06:08 +0100

    Fix movement animation flitting to reset frame

--- Scripts/player_character_body_2d.gd
+++ Scripts/player_character_body_2d.gd
@@ -3,9 +3,15 @@ extends CharacterBody2D
 @export var speed: int = 100
 @onready var anim_tree: AnimationTree = get_node("AnimationTree")
 
+var walking: bool = false
+var last_direction: Vector2
+
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	
+	if velocity != Vector2.ZERO:
+		last_direction = velocity.normalized()
 
 
 func _physics_process(_delta):
@@ -16,10 +22,14 @@ func _physics_process(_delta):
 
 func handle_animation():
 	if velocity == Vector2.ZERO:
-		anim_tree.get('parameters/playback').travel('Idle')
-		
+		walking = false
 	else:
-		anim_tree.get('parameters/playback').travel('Walk')
-		anim_tree.set('parameters/Idle/blend_position', sign(velocity))
-		anim_tree.set('parameters/Walk/blend_position', sign(velocity))
+		walking = true
+
+	anim_tree.set("parameters/conditions/idle", !walking)
+	anim_tree.set("parameters/conditions/walking", walking)
+	
+	if last_direction:
+		anim_tree.set("parameters/Walk/blend_position", last_direction)
+		anim_tree.set("parameters/Idle/blend_position", last_direction)
 	
