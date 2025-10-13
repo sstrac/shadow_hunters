563f29ad245711f5f00a8b844cce36593ef1caf5
mershaughnessy <mershaughnessy@gmail.com>
2025-10-13 12:29:35 +0100

    Created a enemy spawner scene. Creates enemies every 3 seconds. Prototype for future spawners. Currently a bug where the relative position of the player is ofset for the spawner. Causes spawned in enemies to not travel towards the enemies

---
+++ Scripts/enemy_spawner.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export var player_body : Node
+@export var speed : int
+
+const enemy_scene = preload("res://Scenes/Enemy.tscn")
+
+func _ready():
+	$SpawnTimer.start()
+	$SpawnTimer.timeout.connect(_on_spawn_timer_timeout)
+	
+func _on_spawn_timer_timeout():
+	spawn_enemy()
+
+func spawn_enemy():
+	var enemy = enemy_scene.instantiate()
+	enemy.speed = speed
+	enemy.player_body = player_body
+	enemy.position = Vector2(0, 0)
+	print("Enemy has been spawned")
+	add_child(enemy)
